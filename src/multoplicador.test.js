9c8764e0c8372d408df5650009b00be842a83b48
Eduardo Marin Espinoza <emarine@Eduardos-Laptop.local>
2025-03-08 02:16:22 -0400

    Build actualizado y correcciones en package.json

---
+++ src/multoplicador.test.js
@@ -0,0 +1,7 @@
+import multiplicar from "./multiplicadoror.js";
+
+describe("multiplicar", () => {
+  it("deberia multiplicar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(6);
+  });
+});
--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,37 @@
-import sumar from "./sumador";
+import sumar from "./sumador.js";
+import multiplicar from "./multiplicador.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
 
+const sumarButton = document.querySelector("#sumar-button");
+const multiplicarButton = document.querySelector("#Multiplicar-button");
+
+
+sumarButton.addEventListener("click", (event) => {
+  event.preventDefault(); 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  
+  if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+    div.innerHTML = `<p>Resultado: ${sumar(firstNumber, secondNumber)}</p>`;
+  } else {
+    div.innerHTML = "<p>Ingresa números válidos</p>";
+  }
+});
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+
+multiplicarButton.addEventListener("click", (event) => {
+  event.preventDefault(); 
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+  
+  if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+    div.innerHTML = `<p>Resultado: ${multiplicar(firstNumber, secondNumber)}</p>`;
+  } else {
+    div.innerHTML = "<p>Ingresa números válidos</p>";
+  }
 });
